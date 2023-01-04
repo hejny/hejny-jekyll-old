ac4d31b197f7539592f8e61b67b875a132f5033b
Pavol Hejny <me@pavolhejny.com>
2023-01-05 00:45:34 +0100

    Remove italic and bold from project name
    https://chat.openai.com/chat/bb7c3a5b-fe9c-4ccc-9057-f47e0fd66489

--- scripts/generate-projects/removeMarkdownTags.test.ts
+++ scripts/generate-projects/removeMarkdownTags.test.ts
@@ -13,8 +13,36 @@ describe('removeMarkdownTags', () => {
         expect(removeMarkdownTags(str)).toEqual(expected);
     });
 
-    it('should return the original string if it contains no Markdown link tags', () => {
+    it('should remove bold formatting from a string', () => {
+        const str = 'This is **bold** text.';
+        const expected = 'This is bold text.';
+        expect(removeMarkdownTags(str)).toEqual(expected);
+    });
+
+    it('should remove multiple instances of bold formatting from a string', () => {
+        const str = 'This is **bold** and **also bold** text.';
+        const expected = 'This is bold and also bold text.';
+        expect(removeMarkdownTags(str)).toEqual(expected);
+    });
+
+    it('should remove italic formatting from a string', () => {
+        const str = 'This is *italic* text.';
+        const expected = 'This is italic text.';
+        expect(removeMarkdownTags(str)).toEqual(expected);
+    });
+
+    it('should remove multiple instances of italic formatting from a string', () => {
+        const str = 'This is *italic* and *also italic* text.';
+        const expected = 'This is italic and also italic text.';
+        expect(removeMarkdownTags(str)).toEqual(expected);
+    });
+
+    it('should return the original string if it contains no Markdown tags', () => {
         const str = 'This is a plain string.';
         expect(removeMarkdownTags(str)).toEqual(str);
     });
 });
+
+/**
+ * @see https://chat.openai.com/chat/bb7c3a5b-fe9c-4ccc-9057-f47e0fd66489
+ */
